7f363887b3c8cf33712dacf6627928d827cc55b6
omeros <omer.golan.dev@gmail.com>
2021-06-17 21:37:31 +0300

    update images

--- js/main.js
+++ js/main.js
@@ -1,116 +1,28 @@
 'use strict'
 
-// $(document).ready(init);
 
-// function init() {
-//     console.log('Started...');
-//     renderProj()
-// }
-//var x = document.querySelector('row');
-//console.log('nmbm',x);
-
-
-var gstrHTML=   `<div class="col-md-4 col-sm-6 portfolio-item">
-{ <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
-  <div class="portfolio-hover">
-    <div class="portfolio-hover-content">
-      <i class="fa fa-plus fa-3x"></i>
-    </div>
-  </div>
-  <img class="img-fluid" src="img/projs/1.png" alt="">
-</a>
-<div class="portfolio-caption">
-  <h4>Threads</h4>
-  <p class="text-muted">Illustration</p>
-</div>
-</div> `; 
-
-
-
-//`<img src="img\projs\1.png" width= "300" height= "400"  ></img>`;
-
-
-//$('.row2').html(gstrHTML)
-
-
-// var $elBox = $('.row2');
-
-// $elBox.html(` <div class="col-md-4 col-sm-6 portfolio-item">
-//  <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
-//    <div class="portfolio-hover">
-//      <div class="portfolio-hover-content">
-//        <i class="fa fa-plus fa-3x"></i>
-//      </div>
-//    </div>
-//    <img class="img-fluid" src="img/projs/4.png"  alt="">
-//  </a>
-//  <div class="portfolio-caption">
-//    <h4>Threads</h4>
-//    <p class="text-muted">Illustration</p>
-//  </div>
-// </div> `);
-
-
-
-//function renderProject(){
-
-//     `<div class="col-md-4 col-sm-6 portfolio-item">
-//     <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
-//       <div class="portfolio-hover">
-//         <div class="portfolio-hover-content">
-//           <i class="fa fa-plus fa-3x"></i>
-//         </div>
-//       </div>
-//       <img class="img-fluid" src="img/portfolio/01-thumbnail.jpg" alt="">
-//     </a>
-//     <div class="portfolio-caption">
-//       <h4>Threads</h4>
-//       <p class="text-muted">Illustration</p>
-//     </div>
-//   </div> `
-
-
-//}
-
-
-
-
-// `<div class="col-md-4 col-sm-6 portfolio-item">
-// { <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
-//   <div class="portfolio-hover">
-//     <div class="portfolio-hover-content">
-//       <i class="fa fa-plus fa-3x"></i>
-//     </div>
-//   </div>
-//   <img class="img-fluid" src="img/team/1.jpg" alt="">
-// </a>
-// <div class="portfolio-caption">
-//   <h4>Threads</h4>
-//   <p class="text-muted">Illustration</p>
-// </div>
-// </div> `; 
-
- renderProj();
+renderProj();
+renderModal();
 
 function renderProj() {
 
-  var projects = getProjects()
+  const projects = getProjects()
   console.log(projects);
-  var strHTML = projects.map(function(proj) {
+  const strHTML = projects.map(function(proj) {
       return `<div class="col-md-4 col-sm-6 portfolio-item" >
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal1">
-              <div class="portfolio-hover">
-                  <div class="portfolio-hover-content">
-                      <i class="fa fa-plus fa-3x"></i>
+                  <a class="portfolio-link" data-toggle="modal" href="#${proj.modal}">
+                      <div class="portfolio-hover">
+                          <div class="portfolio-hover-content">
+                              <i class="fa fa-plus fa-3x"></i>
+                          </div>
+                      </div>
+                      <img class="img-fluid"  src="img/projs/${proj.id}.png" alt="">
+                  </a>
+                  <div class="portfolio-caption">
+                      <h4>${proj.name}</h4>
+                      <p class="text-muted">${proj.title}</p>
                   </div>
-              </div>
-              <img class="img-fluid"  src="img/projs/${proj.id}.png" alt="">
-          </a>
-          <div class="portfolio-caption">
-              <h4>${proj.name}</h4>
-              <p class="text-muted">${proj.title}</p>
-          </div>
-       </div>`
+              </div>`
   }).join('')
   $('.row2').html(strHTML)
 
@@ -118,5 +30,47 @@ function renderProj() {
 
 
 function renderModal(){
+  const projects = getProjects()
 
+  const strHTML = projects.map(function(proj) {
+    // return  `<div class="portfolio-modal modal fade" id="portfolioModal3" tabindex="-1" role="dialog" aria-hidden="true">
+    return  `<div class="portfolio-modal modal fade" id="${proj.modal}" tabindex="-1" role="dialog" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="close-modal" data-dismiss="modal">
+                      <div class="lr">
+                        <div class="rl"></div>
+                      </div>
+                    </div>
+                    <div class="container">
+                      <div class="row">
+                        <div class="col-lg-8 mx-auto">
+                          <div class="modal-body">
+                            <!-- Project Details Go Here -->
+                            <h2>Project Name</h2>
+                            <p class="item-intro text-muted">Lorem ipsum dolor sit amet consectetur.</p>
+                            <img class="img-fluid d-block mx-auto"  src="img/projs/${proj.id}.png"  alt="">
+                            <p>Use this area to describe your project. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Est blanditiis
+                              dolorem culpa incidunt minus dignissimos deserunt repellat aperiam quasi sunt officia expedita beatae cupiditate,
+                              maiores repudiandae, nostrum, reiciendis facere nemo!</p>
+                            <ul class="list-inline">
+                              <li>Date: January 2017</li>
+                              <li>Client: Threads</li>
+                              <li>Category: Illustration</li>
+                            </ul>
+                            <button class="btn btn-primary" data-dismiss="modal" type="button">
+                                <i class="fa fa-times"></i>
+                                Close Project</button>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                </div>  `
+}).join('')
+$('.modalBox').html(strHTML)
 }
+
+
+
--- js/proj-service.js
+++ js/proj-service.js
@@ -1,30 +1,55 @@
-var gProjs =[ {
-    id: "bookshop",
-     name: "name1", 
-     title: "I am gonna eat you",
-    desc: "zzzzzzzzzzz",
-    url: "projs/sokoban",
-    publishedAt: ' january 28,2021',
-    labels: ["Matrixes", "keyboard events"], 
-},
+const gProjs =[ 
 {
   id: "minsweeper", 
-  name: "name2", 
+  name: "Vanilla js - minsweeper", 
   title: "Better push those boxes",
-  desc: "yyyyyyyyyyyy",
-  url: "projs/sokoban",
+  desc: "Minesweeper is a single-player puzzle video game. The objective of the game is to clear a rectangular board containing hidden mines or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field. The game originates from the 1960s, and it has been written for many computing platforms in use today. It has many variations and offshoots.",
+  url: "n",
   publishedAt: ' january 28,2021',
   labels: ["Matrixes", "keyboard events"], 
+  modal : "portfolioModal1",
+  img : ""
 },
 {
   id: "packman", 
-  name: "name3", 
+  name: "Vanilla js - packman", 
   title: "I am gonna eat you",
-  desc: "xxxxxxxxxxxx",
-  url: "projs/sokoban",
+  desc: "In Pac-Man, the player makes a Pac-Man, a yellow disc, move around a maze. The goal is to eat every yellow pellet (circles) while not getting caught by the ghosts/monsters. For extra points, fruits that appear can also be eaten. When Pac-Man eats a big pellet, the ghosts turn blue and can be eaten. Even though the game has 256 stages, the last level can not be finished due to a problem with the creation of the game.",
+  url: "",
   publishedAt: ' january 28,2021',
   labels: ["Matrixes", "keyboard events"], 
-},   
+  modal : "portfolioModal2"
+},  
+{
+  id: "bookshop",
+  name: "bookshop", 
+  title: "I am gonna eat you",
+  desc: "Welcome to my book shop here you find a wide range of books,get smarter and start read !",
+  url: "",
+  publishedAt: ' january 28,2021',
+  labels: ["Matrixes", "keyboard events"], 
+  modal : "portfolioModal3"
+}, 
+{
+  id: "my-instagram",
+  name: "my-instagram", 
+  title: "I am gonna eat you",
+  desc: "zzzzzzzzzzz",
+  url: "",
+  publishedAt: ' merch ,2021',
+  labels: ["Matrixes", "keyboard events"], 
+  modal : "portfolioModal4"
+},
+{
+  id: "memgen",
+  name: "memgen", 
+  title: "I am gonna eat you",
+  desc: "zzzzzzzzzzz",
+  url: "",
+  publishedAt: ' merch ,2021',
+  labels: ["Matrixes", "keyboard events"], 
+  modal : "portfolioModal5"
+}, 
 ]
 
 
@@ -33,66 +58,36 @@ function getProjects(){
 }
 
 
-/*{ <div class="col-md-4 col-sm-6 portfolio-item">
-<a class="portfolio-link" data-toggle="modal" href="#portfolioModal5">
-  <div class="portfolio-hover">
-    <div class="portfolio-hover-content">
-      <i class="fa fa-plus fa-3x"></i>
-    </div>
-  </div>
-  <img class="img-fluid" src="img/portfolio/05-thumbnail.jpg" alt="">
-</a>
-<div class="portfolio-caption">
-  <h4>aaaaaa</h4>
-  <p class="text-muted">Website Design</p>
-</div>
-</div> }/*
-
-/*{ <div class="col-md-4 col-sm-6 portfolio-item">
-          <a class="portfolio-link" data-toggle="modal" href="#portfolioModal6">
-            <div class="portfolio-hover">
-              <div class="portfolio-hover-content">
-                <i class="fa fa-plus fa-3x"></i>
-              </div>
-            </div>
-            <img class="img-fluid" src="img/portfolio/06-thumbnail.jpg" alt="">
-          </a>
-          <div class="portfolio-caption">
-            <h4>Window</h4>
-            <p class="text-muted">Photography</p>
-          </div>
-        </div> *////}
 
 
-
-        var gProjects = [{
-          id: "bookshop",
-          name: "Pacman",
-          title: "I am gonna eat you",
-          desc: "In Pac-Man, the player makes a Pac-Man, a yellow disc, move around a maze. The goal is to eat every yellow pellet (circles) while not getting caught by the ghosts/monsters. For extra points, fruits that appear can also be eaten. When Pac-Man eats a big pellet, the ghosts turn blue and can be eaten. Even though the game has 256 stages, the last level can not be finished due to a problem with the creation of the game.",
-          projUrl: "",
-          publishedAt: ' january 18,2021 ',
-          labels: ["Matrixes", "keyboard events"],
+      //   var gProjects = [{
+      //     id: "bookshop",
+      //     name: "Pacman",
+      //     title: "I am gonna eat you",
+      //     desc: "In Pac-Man, the player makes a Pac-Man, a yellow disc, move around a maze. The goal is to eat every yellow pellet (circles) while not getting caught by the ghosts/monsters. For extra points, fruits that appear can also be eaten. When Pac-Man eats a big pellet, the ghosts turn blue and can be eaten. Even though the game has 256 stages, the last level can not be finished due to a problem with the creation of the game.",
+      //     projUrl: "",
+      //     publishedAt: ' january 18,2021 ',
+      //     labels: ["Matrixes", "keyboard events"],
       
-      }, {
-          id: "minsweeper",
-          name: "MineSweeper",
-          title: "beware",
-          desc: "Minesweeper is a single-player puzzle video game. The objective of the game is to clear a rectangular board containing hidden mines or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field. The game originates from the 1960s, and it has been written for many computing platforms in use today. It has many variations and offshoots.",
-          projUrl: "",
-          publishedAt: ' january 21,2021',
-          labels: ["Matrixes", "keyboard events"],
+      // }, {
+      //     id: "minsweeper",
+      //     name: "MineSweeper",
+      //     title: "beware",
+      //     desc: "Minesweeper is a single-player puzzle video game. The objective of the game is to clear a rectangular board containing hidden mines or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field. The game originates from the 1960s, and it has been written for many computing platforms in use today. It has many variations and offshoots.",
+      //     projUrl: "",
+      //     publishedAt: ' january 21,2021',
+      //     labels: ["Matrixes", "keyboard events"],
       
-      }, {
-          id: "packman",
-          name: "BookShop",
-          title: "eat them",
-          desc: "Welcome to my book shop here you find a wide range of books,get smarter and start read !",
-          projUrl: "",
-          publishedAt: ' january 28,2021',
-          labels: ["Matrixes", "keyboard events"],
+      // }, {
+      //     id: "packman",
+      //     name: "BookShop",
+      //     title: "eat them",
+      //     desc: "Welcome to my book shop here you find a wide range of books,get smarter and start read !",
+      //     projUrl: "",
+      //     publishedAt: ' january 28,2021',
+      //     labels: ["Matrixes", "keyboard events"],
       
-      }, ]
+      // }, ]
       
 
     //   function getProjects() {
