92a8dca277d1c3bdc5c1874f424f4c0d61585876
omeros <omer.golan.dev@gmail.com>
2021-08-01 18:33:54 +0300

    favicon change

--- js/proj-service.js
+++ js/proj-service.js
@@ -2,7 +2,7 @@ const gProjs =[
 {
     id: "my-instagram",
     name: "my-instagram", 
-    title: "creat a new user profile, post your photos and use the chat to talk with other users ",
+    title: "choose a user or creat a new a one, post your photos and use the chat to talk with other users ",
     desc: "End-to-End CRUD social networking site, with Vuex, async programming, Web Sockets ,Users, Node.js, MongoDB,",
     url: "https://my-cloudy-instagram-app.herokuapp.com/",
     Category:"Vue.js",  
@@ -49,38 +49,5 @@ function getProjects(){
 }
 
 
-
-
-      //   var gProjects = [{
-      //     id: "bookshop",
-      //     name: "Pacman",
-      //     title: "I am gonna eat you",
-      //     desc: "In Pac-Man, the player makes a Pac-Man, a yellow disc, move around a maze. The goal is to eat every yellow pellet (circles) while not getting caught by the ghosts/monsters. For extra points, fruits that appear can also be eaten. When Pac-Man eats a big pellet, the ghosts turn blue and can be eaten. Even though the game has 256 stages, the last level can not be finished due to a problem with the creation of the game.",
-      //     projUrl: "",
-      //     publishedAt: ' january 18,2021 ',
-      //     labels: ["Matrixes", "keyboard events"],
       
-      // }, {
-      //     id: "minsweeper",
-      //     name: "MineSweeper",
-      //     title: "beware",
-      //     desc: "Minesweeper is a single-player puzzle video game. The objective of the game is to clear a rectangular board containing hidden mines or bombs without detonating any of them, with help from clues about the number of neighboring mines in each field. The game originates from the 1960s, and it has been written for many computing platforms in use today. It has many variations and offshoots.",
-      //     projUrl: "",
-      //     publishedAt: ' january 21,2021',
-      //     labels: ["Matrixes", "keyboard events"],
-      
-      // }, {
-      //     id: "packman",
-      //     name: "BookShop",
-      //     title: "eat them",
-      //     desc: "Welcome to my book shop here you find a wide range of books,get smarter and start read !",
-      //     projUrl: "",
-      //     publishedAt: ' january 28,2021',
-      //     labels: ["Matrixes", "keyboard events"],
-      
-      // }, ]
-      
-
-    //   function getProjects() {
-    //     return gProjects
-    // }
+   
